b658ea986813b9d6661d891333c427672f009a89
Fu Wu Zhong Xin <luohujinan@tom.com>
2018-12-17 01:36:29 +0800

    'guanlichu'

---
+++ git_auto_pull_push - 副本.bat
@@ -0,0 +1,15 @@
+set PATH=D:\owners_committee\tools\PortableGit\bin;%PATH%
+D:
+
+
+#git clone https://github.com/LuoHuJinAn/home.git
+
+
+cd D:\owners_committee\public\home
+git pull
+git add -A
+git commit -a -m 'guanlichu'
+git push origin master
+
+pause
+
