faf5e4308ef377bb6b89123cfec3b1a1db1a7c87
Fu Wu Zhong Xin <luohujinan@tom.com>
2018-12-17 14:34:11 +0800

    Fu Wu Zhong Xin

--- tools/auto_pull_push.bat
+++ tools/auto_pull_push.bat
@@ -4,11 +4,15 @@ D:
 
 rem cd D:\owners_committee\public
 rem git clone https://github.com/LuoHuJinAn/home.git
+
+rem cd D:\owners_committee\public\home
 rem git config user.name "Fu Wu Zhong Xin"
 rem git config user.email "luohujinan@tom.com"
 
 
 cd D:\owners_committee\public\home
+
+
 git pull
 git add -A
 git commit -a -m "Fu Wu Zhong Xin"
