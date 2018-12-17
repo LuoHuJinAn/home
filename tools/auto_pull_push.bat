352eed14a280cfd37d618e082cd546260e9dd530
MTRCORP\neoni <neoni@mtr.com.hk>
2018-12-17 14:06:09 +0800

    Fu Wu Zhong Xin

---
+++ tools/auto_pull_push.bat
@@ -0,0 +1,17 @@
+set PATH=D:\owners_committee\tools\PortableGit\bin;%PATH%
+D:
+
+
+rem cd D:\owners_committee\public
+rem git clone https://github.com/LuoHuJinAn/home.git
+rem git config user.name "Fu Wu Zhong Xin"
+rem git config user.email "luohujinan@tom.com"
+
+
+cd D:\owners_committee\public\home
+git pull
+git add -A
+git commit -a -m "Fu Wu Zhong Xin"
+git push origin master
+
+rem pause
