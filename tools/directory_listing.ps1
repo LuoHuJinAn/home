deb5601c70446abb849d7f7a191b460d3229a35b
Fu Wu Zhong Xin <luohujinan@tom.com>
2019-01-24 09:48:05 +0800

    Fu Wu Zhong Xin

---
+++ tools/directory_listing.ps1
@@ -0,0 +1,5 @@
+﻿
+
+
+Get-ChildItem "C:\Folder_Name\" | Where-Object {$_.Extension -eq ".pdf"} | Select-Object @{name="Link"; expression={"a href='$($_.FullName)'>$($_.Name)</a>"}} | Out-File C:\FileName.html
+
