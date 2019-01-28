f6ad63405ad38bf02fd28f060f169d2b6a2a91ac
Fu Wu Zhong Xin <luohujinan@tom.com>
2019-01-28 14:06:09 +0800

    Fu Wu Zhong Xin

--- tools/directory_listing.ps1
+++ tools/directory_listing.ps1
@@ -3,7 +3,13 @@
 
 Get-ChildItem "D:\owners_committee\public\home\from_services_centre\letters" | Sort-Object -Property Name -Descending | Select-Object @{name="函件"; expression={"* [$($_.Name)](./letters/$($_.Name))"}} | Out-File "D:\owners_committee\public\home\from_services_centre\letters_index.md"
 
-Get-ChildItem "D:\owners_committee\public\home\from_services_centre\letters" | Sort-Object -Property Name -Descending | Select-Object @{name="函件"; expression={"* [$($_.Name)](./letters/$($_.Name))"}} 
+
+$myFiles = Get-ChildItem "D:\owners_committee\public\home\from_services_centre\letters" | Sort-Object -Property Name -Descending | Select-Object @{name="函件"; expression={"* [$($_.Name)](./letters/$($_.Name))"}}
+
+$myFiles
+
 
 # Get-ChildItem "D:\owners_committee\public\home\from_services_centre\letters" | Select-Object @{name="函件"; expression={"* [$($_.Name)](./letters/$($_.Name))"}} | Sort -desc | Out-File "D:\owners_committee\public\home\from_services_centre\letters.md"
+
+
 
